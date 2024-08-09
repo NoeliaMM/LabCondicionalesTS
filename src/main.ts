a879928a69b4b0a0067ad81ebf977dd979633f0b
NoeMM <mm.noe2011@gmail.com>
2024-08-09 21:23:21 +0200

    fin

--- src/main.ts
+++ src/main.ts
@@ -1,126 +1,156 @@
 import "./style.css";
 
-let puntuacion : number = 0;
-const min :number = 1;
-const max:number = 12;
+let puntos: number = 0;
+let cartas_excluidas: Array<number> = [8, 9];
+const min: number = 1;
+const max: number = 12;
 type OpcionesCartas = {
-  [key: number]: string
+  [key: number]: string;
 };
 
-const mapeoCarta : OpcionesCartas ={
-  1:'1_as',
-  2:'2_dos',
-  3:'3_tres',
-  4:'4_cuatro',
-  5:'5_cinco',
-  6:'6_seis',
-  7:'7_siete',
-  10:'10_sota',
-  11:'11_caballo',
-  12:'12_rey'
+const mapeoCarta: OpcionesCartas = {
+  1: "1_as",
+  2: "2_dos",
+  3: "3_tres",
+  4: "4_cuatro",
+  5: "5_cinco",
+  6: "6_seis",
+  7: "7_siete",
+  10: "10_sota",
+  11: "11_caballo",
+  12: "12_rey",
 };
 
 type Estado =
- | "CONSERVADOR"
- | "CANGUELO"
- | "CASI"
- | "CLAVADO"
- | "GAME_OVER";
+  | "CONSERVADOR"
+  | "CANGUELO"
+  | "CASI"
+  | "CLAVADO"
+  | "PASADO"
+  | "GAME_OVER";
 
- const PUNTUACION_CONSERVADORA: number = 4;
- const PUNTUACION_CANGUELO: number = 5;
- const PUNTUACION_CASI: number = 6;
- const PUNTUACION_CLAVADO: number = 7.5;
- 
+const PUNTUACION_CONSERVADORA: number = 4;
+const PUNTUACION_CANGUELO: number = 5;
+const PUNTUACION_CASI: number = 6;
+const PUNTUACION_CLAVADO: number = 7.5;
 
-
- const mostrarMensaje = (estado)=>{
+const mostrarMensaje = (estado: Estado) => {
   let mensaje = "";
   switch (estado) {
     case "CONSERVADOR":
-    mensaje = `Has sido muy conservador`;
-    break;
+      mensaje = `Has sido muy conservador 🤔`;
+      break;
     case "CANGUELO":
-    mensaje = `Te ha entrado el canguelo eh?`;
-    break;
+      mensaje = `Te ha entrado el canguelo eh? 😏`;
+      break;
     case "CASI":
-    mensaje = `Casi casi...`;
-    break;
+      mensaje = `Casi casi... 🥲`;
+      break;
     case "CLAVADO":
-    mensaje = `¡ Lo has clavado! ¡Enhorabuena! 🎉🎉🎉`;
-    break;
+      mensaje = `¡ Lo has clavado! ¡Enhorabuena! 🎉🎉🎉`;
+      break;
     case "GAME_OVER":
-    mensaje = `🪦 Partida terminada`;
-    break;
+      mensaje = `🪦 Partida terminada`;
+      break;
+    case "PASADO":
+      mensaje = `Te has pasado 😬`;
+      break;
     default:
-    mensaje = "No se que ha pasado, pero no deberías estar aquí";
-    break;
-    }
-    const resultado = document.getElementById("resultado");
-    if(resultado){
-      resultado.innerHTML = mensaje;
-    }   
- }
-
- const comprobarPuntuacion = () :void =>{
-
-  if(puntuacion < PUNTUACION_CLAVADO ){
-// TO DO SEGUIR POR AQUI
+      mensaje = "No se que ha pasado, pero no deberías estar aquí";
+      break;
   }
- }
+  const resultado = document.getElementById("resultado");
+  if (resultado) {
+    resultado.innerHTML = mensaje;
+  }
+};
 
+const comprobarPuntuacion = (): void => {
+  finPartida();
+  switch (true) {
+    case puntos === PUNTUACION_CLAVADO:
+      mostrarMensaje("CLAVADO");
+      break;
 
-const mapearValorCarta = (carta :number): number => {
+    case puntos === PUNTUACION_CONSERVADORA:
+      mostrarMensaje("CONSERVADOR");
+      break;
+    case puntos === PUNTUACION_CASI:
+      mostrarMensaje("CASI");
+      break;
 
-  if([10,11,12].includes(carta)){
+    case puntos === PUNTUACION_CANGUELO:
+      mostrarMensaje("CANGUELO");
+      break;
+
+    case puntos > PUNTUACION_CLAVADO:
+      mostrarMensaje("PASADO");
+      break;
+
+    default:
+      mostrarMensaje("GAME_OVER");
+      break;
+  }
+};
+
+const finPartida = (): void => {
+  if (btnPedirCarta && btnPedirCarta instanceof HTMLButtonElement) {
+    btnPedirCarta.disabled = true;
+  }
+  if (btnPararPartida && btnPararPartida instanceof HTMLButtonElement) {
+    btnPararPartida.disabled = true;
+  }
+};
+
+const mapearValorCarta = (carta: number): number => {
+  if ([10, 11, 12].includes(carta)) {
     return 0.5;
   }
   return carta;
-}
+};
 
-const muestraCarta=(carta:number): void=>{
-
+const muestraCarta = (carta: number): void => {
   const imgCarta = document.getElementById("carta");
-  if(imgCarta && imgCarta instanceof HTMLImageElement){
+  if (imgCarta && imgCarta instanceof HTMLImageElement) {
     imgCarta.src = `https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/${mapeoCarta[carta]}-copas.jpg`;
     imgCarta.alt = `${carta} de copas`;
-  }else{
+  } else {
     console.error(`No se ha encontrado el elemento con id carta`);
   }
-
-}
+};
 
 const muestraPuntuacion = () => {
-
   const puntuacionItem = document.getElementById("puntuacion");
 
-  if(puntuacionItem  && puntuacionItem instanceof HTMLInputElement ){
-     puntuacionItem.value = puntuacion.toString();
-  }else{
+  if (puntuacionItem && puntuacionItem instanceof HTMLInputElement) {
+    puntuacionItem.value = puntos.toString();
+  } else {
     console.error(`No se ha encontrado el elemento con id puntuacion`);
   }
-}
+};
 
-const dameCarta = ():void => {
+const dameCarta = (): void => {
   const cartaRandom = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  if([8,9].includes(cartaRandom)){
+  if (cartas_excluidas.includes(cartaRandom)) {
     dameCarta();
+    return;
   }
-  
-  puntuacion += mapearValorCarta(cartaRandom);
+  cartas_excluidas.push(cartaRandom);
+
+  puntos += mapearValorCarta(cartaRandom);
   muestraCarta(cartaRandom);
   muestraPuntuacion();
-  comprobarPuntuacion();
-  
-}
-
+  if (puntos >= PUNTUACION_CLAVADO) {
+    comprobarPuntuacion();
+  }
+};
 
 document.addEventListener("DOMContentLoaded", muestraPuntuacion);
 const btnPedirCarta = document.getElementById("pedirCarta");
 btnPedirCarta?.addEventListener("click", dameCarta);
 
+const btnPararPartida = document.getElementById("pararPartida");
+btnPararPartida?.addEventListener("click", comprobarPuntuacion);
 
-
-
-
+const btnReset = document.getElementById("reset");
+btnReset?.addEventListener("click", () => location.reload());
